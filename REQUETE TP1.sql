4e9706211a28c33c21c3886f024b30f215dbaea6
TonNom <ton.email@example.com>
2025-09-16 00:12:09 +0200

    'Q6--Q11'

--- REQUETE TP1.sql
+++ REQUETE TP1.sql
@@ -18,3 +18,54 @@ FROM pg_stat_activity
 GROUP BY usename
 HAVING COUNT(DISTINCT datname) > 2;
 
+-- 6. Comptes connectés sur une base qui n’est pas la leur (hors postgres)
+SELECT usename, datname
+FROM pg_stat_activity
+WHERE datname NOT IN ('postgres') 
+  AND usename <> datname;
+
+-- 7. Nombre de connexions simultanées autorisées pour le compte m1user3_n
+SELECT rolname, rolconnlimit
+FROM pg_roles
+WHERE rolname = 'm1user3_n';
+
+-- 8. Nombre de connexions simultanées autorisées sur la base m1_circuit_n
+SELECT datname, datconnlimit
+FROM pg_database
+WHERE datname = 'm1_circuit_n';
+
+-- 9. Contraintes de domaine des attributs des tables de la base
+SELECT table_name, column_name, data_type, is_nullable, column_default
+FROM information_schema.columns
+WHERE table_schema = 'public';
+
+-- 10a. Taille du cache mémoire
+SHOW shared_buffers;
+
+-- 10b. Taille maximale des noms des attributs
+SHOW max_identifier_length;
+
+-- 10c. Fuseau horaire utilisé par le serveur
+SHOW TimeZone;
+
+-- 10d. Temps au bout duquel un deadlock est identifié
+SHOW deadlock_timeout;
+
+-- 10e. Nombre maximal de connexions simultanées sur le serveur
+SHOW max_connections;
+
+-- 11a. Triggers présents dans la base
+SELECT trigger_name, event_object_table, event_manipulation, action_statement
+FROM information_schema.triggers
+WHERE trigger_schema = 'public';
+
+-- 11b. Droits (privilèges) des différents utilisateurs sur les tables de la base
+SELECT grantee, privilege_type, table_name
+FROM information_schema.role_table_grants
+WHERE table_schema = 'public';
+
+SELECT table_schema, table_name
+FROM information_schema.tables
+WHERE table_schema = 'public'  -- pour ton schéma principal
+  AND table_type = 'BASE TABLE';  -- uniquement les tables (pas les vues)
+SELECT * FROM reservation;
