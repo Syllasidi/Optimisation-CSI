0ff1d6347fd97859c06664c435df1a073bc2a3d5
TonNom <ton.email@example.com>
2025-09-16 00:08:46 +0200

    'Q2'

--- REQUETE TP1.sql
+++ REQUETE TP1.sql
@@ -2,3 +2,6 @@
 -- 1 les bases installées sur le serveur 
 SELECT datname, datdba, encoding,pg_encoding_to_char(encoding) AS encodage ,datistemplate FROM pg_database;
 
+-- 2 es comptes présents sur le serveur
+SELECT rolname, rolsuper, rolcreatedb, rolcanlogin
+FROM pg_roles;
