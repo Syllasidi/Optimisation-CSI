f059a302a8ccab4575db9401a8d927876e988ea7
TonNom <ton.email@example.com>
2025-09-16 00:09:58 +0200

    'Q3'

--- REQUETE TP1.sql
+++ REQUETE TP1.sql
@@ -5,3 +5,6 @@ SELECT datname, datdba, encoding,pg_encoding_to_char(encoding) AS encodage ,dati
 -- 2 es comptes présents sur le serveur
 SELECT rolname, rolsuper, rolcreatedb, rolcanlogin
 FROM pg_roles;
+-- 3 Quels comptes peuvent créer des bases
+SELECT rolname, rolsuper, rolcreatedb, rolcanlogin
+FROM pg_roles WHERE rolcreatedb = 'true';
